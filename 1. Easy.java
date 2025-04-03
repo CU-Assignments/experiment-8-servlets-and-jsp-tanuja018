7ca839a03d8611e2f55e3014b36d686dbc77d775
tanuja018 <tanujarao571@gmail.com>
2025-04-03 12:06:15 +0530

    Create 1. Easy.java

---
+++ 1. Easy.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if("admin".equals(username) && "admin123".equals(password)) {
+            out.println("<h2>Welcome, " + username + "!</h2>");
+        } else {
+            out.println("<h2>Invalid credentials. Try again.</h2>");
+        }
+    }
+}
+
+//HTML Form for Login:
+<form action="LoginServlet" method="post">
+    Username: <input type="text" name="username"><br>
+    Password: <input type="password" name="password"><br>
+    <input type="submit" value="Login">
+</form>
